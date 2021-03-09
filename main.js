8fdb954d154da3543b3356587291b5674bf79229
Dora Budić <dbudic0706@gmail.com>
2021-03-09 16:36:09 +0100

    Bonus - maxLimit changes, modified range in score function

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 var bombsArray = [];
 var playerNumbers = [];
-maxLimit = 100;
+var maxLimit;
+
+// Chiedere all'utente una difficoltà
+var control = true;
+
+while (control) {
+  var level = parseInt(prompt("Choose level of difficulty: 0 (range 1-100), 1 (1-80) or 2 (1-50)"));
+  switch (level) {
+    case 0:
+    maxLimit = 100;
+    control = false;
+    break;
+
+    case 1:
+    maxLimit = 80;
+    control = false;
+    break;
+
+    case 2:
+    maxLimit = 50;
+    control = false;
+    break;
+
+    default:
+    console.log("You have to choose or 0 or 1 or 2!!");
+    break;
+  }
+}
 
 // Chiamo la funzione per 16 numeri random
 var bombsArray = createBombs(bombsArray, maxLimit);
@@ -8,7 +35,7 @@ document.getElementById('computer').innerHTML = "Positions with a bomb: " + bomb
 
 // Chiamo la funzione per avere il punteggio dell'utente
 var result = score(bombsArray, playerNumbers, maxLimit);
-document.getElementById('score').innerHTML = "User has ended the game with " + result + " points";
+document.getElementById('score').innerHTML = "User has ended the game with " + result + " points.";
 
 // Funzione - genera 16 numeri random tra 1 e 100, senza numeri doppi
 function createBombs(array,max) {
@@ -31,12 +58,12 @@ function randomNumber(min,max) {
   }
 }
 
-// Chiedo all'utente di inserire un numero tra 1 e 100, non puo ripetere lo stesso numero
+// Chiedo all'utente di inserire un numero tra 1 e max (100, 80, 50), non puo ripetere lo stesso numero
 // Funzione - calcola il numero di volte che l’utente ha inserito un numero consentito
 function score(generatedNumbers, insertedNumbers, max) {
   while (insertedNumbers.length < max - 16) {
     var number = parseInt(prompt("Write one number between 1 and " + max));
-    if (number >= 1 && number <= 100 && !isNaN(number) && !insertedNumbers.includes(number)) {
+    if (number >= 1 && number <= max && !isNaN(number) && !insertedNumbers.includes(number)) {
       if (!generatedNumbers.includes(number)) {
         insertedNumbers.push(number);
       } else {
